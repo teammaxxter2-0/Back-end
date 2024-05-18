c5aa8a9ab791dea77b4c54098c76fd5ed44045d5
Luuk <luuk.volkering@gmail.com>
2024-05-18 17:01:42 +0200

    Error in het item model gefixt

--- OfferteApp/Models/Item.cs
+++ OfferteApp/Models/Item.cs
@@ -2,6 +2,7 @@ namespace OfferteApp.Models;
 
 public class Item
 {
+    public int Id { get; set; }
     public string Description { get; set; }
     public int Quantity { get; set; }
     public decimal PricePerUnit { get; set; }
